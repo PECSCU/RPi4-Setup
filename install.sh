5727b070201464fb9e3da610e5db2fd233f50586
Steven Owen <steven@9chd.com>
2022-09-04 18:14:27 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,35 @@
 #!/bin/sh
 
-
+cd ~
 sudo apt-get update
 sudo apt-get upgrade -y
+sudo apt dist-upgrade
+suo apt clean
 sudo apt-get install build-essential python-dev python-smbus git - y
 
-cd
+cd ~
 git clone https://github.com/adafruit/Adafruit_Python_ADS1x15.git
 cd Adafruit_Python_ADS1x15
 sudo python setup.py install -y
 
-cd
+cd ~
 git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
 cd Adafruit_Python_GPIO
 sudo python3 setup.py install -y
 
-cd
+cd ~
 pip3 install setuptools
 pip3 install wheel
 pip3 install adafruit-io
-sudo apt install mono-complete -y
+sudo apt install mono-complete
 
-cd
+cd ~
 mkdir Programs
 cd Programs
 
 git clone https://github.com/PECSCU/pythonScripts.git
 
-cd
+cd ~
 cd Programs
 git clone https://github.com/PECSCU/AtMoS.git
 
